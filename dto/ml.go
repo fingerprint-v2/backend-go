39739a701bf445963a007d2a4fe285e24a84f78b
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-02-20 09:51:42 +0700

    Update dto

--- dto/ml.go
+++ dto/ml.go
@@ -1,6 +1,7 @@
 package dto
 
 type CreateTrainingReq struct {
+	SiteID       string `json:"site_id" validate:"required,uuid4"`
 	TrainingName string `json:"training_name" validate:"required"`
 	TrainingType string `json:"training_type" validate:"required,oneof=SUPERVISED UNUPERVISED"`
 }

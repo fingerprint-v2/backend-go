ec9896ffe34eb673c45cc5919eda2e6ad6feb989
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-02-24 07:37:26 +0700

    Add training mock

--- ml/src/services/trainer.py
+++ ml/src/services/trainer.py
@@ -1,6 +1,16 @@
+import random
+import time
+
+
 class Trainer:
     def __init__(self):
         print("Trainer created")
 
     def train(self, data):
-        print(data)
+        id_random = random.randint(1000, 99999)
+        print(f"Training the model: {id_random}....")
+        # print(data)
+        for i in range(5):
+            print(f"{id_random}: Epoch {i+1}...")
+            time.sleep(1)
+        print(f"Model trained: {id_random}")
